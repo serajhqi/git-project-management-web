feb1528d4bdf2e8ee8f7e8ff9b93a5f9b668ff70
Seraj Haqiqi <haqiqi.seraj@gmail.com>
2024-11-20 18:40:12 +0330

    feat: add base styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import './App.css'
+import { Container, Content, Header } from 'rsuite'
 
 function App() {
 
   return (
-    <>
-      Projects:
-      <div className="flex gap-3">
-        <a href='/project/admin-service'>Admin Service</a>
-        <a href='/project/price-service'>Price Service</a>
-      </div>
+    <Container className='w-full h-full'>
+      <Header className='bg-red-100'>Header</Header>
+      <Content className='p-4'>
 
-      Some Reports are here from the:
-      Time, number of tasks and activities that each user has
-    </>
+        Projects:
+        <div className="flex gap-3">
+          <a href='/project/admin-service'>Admin Service</a>
+          <a href='/project/price-service'>Price Service</a>
+        </div>
+
+        Some Reports are here from the:
+        Time, number of tasks and activities that each user has
+
+      </Content>
+
+    </Container>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import {
-  RouterProvider,
-} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { router } from './router';
-
+import 'rsuite/dist/rsuite.min.css';
+import './index.css'
 
 
 createRoot(document.getElementById('root')!).render(
